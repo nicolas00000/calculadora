b0775867ee61d4dc7ad296ea5463aca32e280663
Nicolas Mariano <nicmariano10@gmail.com>
2022-08-18 00:52:55 -0300

    travando e sempre deixando o resultado em zero se tentar apagar tudo

--- assests/script.js
+++ assests/script.js
@@ -14,11 +14,12 @@ function apagar(){
 }
 
 function back(){
-    resultados.innerHTML = "00"
-    travar= true
     const resultado = document.querySelector(".resultado").innerHTML    
     document.querySelector(".resultado").innerHTML = resultado.substring(0 , resultado.length - 1)
-    
+    if(resultados.innerHTML === ""){
+        resultados.innerHTML = "0"
+        travar = true
+    }
 
 }
 
